4c751e1de12a81ec0595451bb804c2f5edd10452
hyeonji0409 <guswl8507@naver.com>
2024-05-14 23:33:26 +0900

    기본생성자

---
+++ src/construct/MemberDefault.java
@@ -0,0 +1,12 @@
+package construct;
+
+// 기본 생성자 확인
+public class MemberDefault {
+
+    String name;
+
+    MemberDefault() {
+        System.out.println("생성자 호출");
+    }
+
+}
--- src/construct/MemberInit.java
+++ src/construct/MemberInit.java
@@ -5,6 +5,10 @@ public class MemberInit {
     int age;
     int grade;
 
+    MemberInit() { // 생성자 필요
+
+    }
+
     // Main3 사용을 위한 추가
     void initMember(String name, int age, int grade){
         this.name = name;
